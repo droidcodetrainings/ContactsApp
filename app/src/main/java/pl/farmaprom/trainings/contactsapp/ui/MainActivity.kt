a86133f619ab1cab8e700accf332d4c40660f6c6
Kamil Ruchala <kamil.ruchala@farmaprom.pl>
2024-04-15 11:26:17 +0200

    #1: HeaderItem and ContactItem

--- app/src/main/java/pl/farmaprom/trainings/contactsapp/ui/MainActivity.kt
+++ app/src/main/java/pl/farmaprom/trainings/contactsapp/ui/MainActivity.kt
@@ -3,26 +3,41 @@ package pl.farmaprom.trainings.contactsapp.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import pl.farmaprom.trainings.contactsapp.ui.theme.ContactsAppTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ContactsAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.surface
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
                 ) {
-                    Greeting("Android")
+                    Column(
+                        modifier = Modifier.padding(it),
+                        verticalArrangement = Arrangement.SpaceEvenly
+                    ) {
+                        HeaderItem(text = "Test")
+                        for (i in 1..10) {
+                            ContactItem(
+                                imageUrl = null,
+                                name = "contact $i",
+                                isFavourite = i%4 ==
+                                        0
+                            )
+                        }
+                    }
                 }
             }
         }
@@ -38,6 +53,6 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     ContactsAppTheme {
-        Greeting("Android")
+        Greeting("Android sssshshshsh")
     }
 }
