fe3899dc42ecaf0013e85dbefdb6be13414adebd
tomaszmaslon <tomasz.maslon@gmail.com>
2025-04-04 12:58:44 +0200

    1: Showcasing arrangements and alignment in Column

--- app/src/main/java/pl/farmaprom/trainings/contactsapp/ui/MainActivity.kt
+++ app/src/main/java/pl/farmaprom/trainings/contactsapp/ui/MainActivity.kt
@@ -3,13 +3,22 @@ package pl.farmaprom.trainings.contactsapp.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import pl.farmaprom.trainings.contactsapp.ui.theme.ContactsAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,11 +27,13 @@ class MainActivity : ComponentActivity() {
         setContent {
             ContactsAppTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
+                Scaffold(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.surface
                 ) {
-                    Greeting("Android")
+                    Greeting(
+                        modifier = Modifier.padding(it),
+                        name = "Android"
+                    )
                 }
             }
         }
@@ -30,8 +41,26 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Greeting(
+    name: String,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp)
+            .fillMaxHeight(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            modifier = modifier
+                .padding(16.dp)
+                .clickable { },
+            text = "Hello $name!"
+        )
+        Text("Having fun?")
+    }
 }
 
 @Preview(showBackground = true)
