c674b60a2a6edf359b79287acddec6a5d6e3066f
Kamil Ruchala <kamil.ruchala@farmaprom.pl>
2025-05-16 11:16:23 +0200

    navigation compose

--- app/src/main/java/pl/farmaprom/trainings/contactsapp/contacts/list/presentation/ContactsListView.kt
+++ app/src/main/java/pl/farmaprom/trainings/contactsapp/contacts/list/presentation/ContactsListView.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 import pl.farmaprom.trainings.contactsapp.R
 import pl.farmaprom.trainings.contactsapp.contacts.data.Contact
 import pl.farmaprom.trainings.contactsapp.contacts.utils.generateContacts
--- app/src/main/java/pl/farmaprom/trainings/contactsapp/ui/MainActivity.kt
+++ app/src/main/java/pl/farmaprom/trainings/contactsapp/ui/MainActivity.kt
@@ -1,7 +1,9 @@
 package pl.farmaprom.trainings.contactsapp.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -18,8 +20,15 @@ import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
+import kotlinx.serialization.Serializable
+import pl.farmaprom.trainings.contactsapp.contacts.data.Contact
 import pl.farmaprom.trainings.contactsapp.contacts.list.presentation.ContactsListView
 import pl.farmaprom.trainings.contactsapp.contacts.list.presentation.ContactsViewState
+import pl.farmaprom.trainings.contactsapp.contacts.preview.ContactPreviewScreen
 import pl.farmaprom.trainings.contactsapp.contacts.utils.generateContacts
 import pl.farmaprom.trainings.contactsapp.ui.theme.ContactsAppTheme
 
@@ -30,21 +39,65 @@ class MainActivity : ComponentActivity() {
             ContactsAppTheme {
                 val viewModel = viewModel<MainViewModel>()
                 val contactsViewState by viewModel.contactsViewState.collectAsStateWithLifecycle()
-                // A surface container using the 'background' color from the theme
-                ContactsView(
-                    contactsViewState = contactsViewState
-                )
+                val selectedContact = contactsViewState.selectedContact
+//                BackHandler(enabled = selectedContact != null) {
+//                    viewModel.onContactSelected(null)
+//                }
+//                if (selectedContact != null) { // contactsViewState.selectedContact?.let { } ?: { }
+//                    ContactPreviewScreen(
+//                        contact = selectedContact,
+//                        onBackClick = {
+//                            viewModel.onContactSelected(null)
+//                        }
+//                    )
+//                } else {
+//                    ContactsView(
+//                        contactsViewState = contactsViewState
+//                    )
+//                }
+
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = List
+                ) {
+                    composable<List> {
+                        ContactsView(
+                            contactsViewState = contactsViewState,
+                            onContactClick = { contact ->
+                                viewModel.onContactSelected(contact)
+                                navController.navigate(ContactPreview(id = contact.id))
+                            }
+                        )
+                    }
+                    composable<ContactPreview> {
+                        val resolved = it.toRoute<ContactPreview>()
+                        Log.d("KRTEST", "resolved args: $resolved")
+                        ContactPreviewScreen(
+                            contact = selectedContact!!, // TODO pass id of contact
+                            onBackClick = {
+                                viewModel.onContactSelected(null)
+                            }
+                        )
+                    }
+                }
             }
         }
     }
 }
 
+@Serializable
+data object List
+
+@Serializable
+data class ContactPreview(val id: Long)
+
 @Composable
 fun ContactsView(
     modifier: Modifier = Modifier,
-    contactsViewState: ContactsViewState
+    contactsViewState: ContactsViewState,
+    onContactClick: (Contact) -> Unit = {}
 ) {
-    val viewModel = viewModel<MainViewModel>()
     Scaffold(
         modifier = Modifier.fillMaxSize(),
         topBar = {}
@@ -57,9 +110,7 @@ fun ContactsView(
             ContactsListView(
                 modifier = modifier.padding(padding),
                 contactsViewState = contactsViewState,
-                onContactClick = { contact ->
-                    viewModel.onContactSelected(contact)
-                }
+                onContactClick = onContactClick
             )
         }
     }
