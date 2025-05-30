917b482e256d0b9405ba2a7383b30c91350dc0e2
Kamil Ruchala <kamil.ruchala@farmaprom.pl>
2025-05-30 13:23:10 +0200

    get data from api using retrofit

--- app/src/main/java/pl/farmaprom/trainings/contactsapp/contacts/presentation/list/MainViewModel.kt
+++ app/src/main/java/pl/farmaprom/trainings/contactsapp/contacts/presentation/list/MainViewModel.kt
@@ -2,21 +2,58 @@ package pl.farmaprom.trainings.contactsapp.contacts.presentation.list
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import pl.farmaprom.trainings.contactsapp.contacts.data.Contact
+import pl.farmaprom.trainings.contactsapp.contacts.utils.generateContact
 import pl.farmaprom.trainings.contactsapp.contacts.utils.generateContacts
+import pl.farmaprom.trainings.contactsapp.dogapi.DogApiService
+import retrofit2.Retrofit
 
 class MainViewModel : ViewModel() {
     private val _contactsViewState = MutableStateFlow(ContactsViewState(emptyList()))
     val contactsViewState = _contactsViewState.asStateFlow()
 
+    private val loggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .build()
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl("https://dogapi.dog/api/v2/")
+        .client(okHttpClient)
+        .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
+        .build()
+    private val dogApiService: DogApiService = retrofit.create(DogApiService::class.java)
+
     init {
         Log.d("MainViewModel", "init")
-        _contactsViewState.value = ContactsViewState(generateContacts(100))
+        refresh()
+    }
+
+    private fun refresh() {
+        viewModelScope.launch {
+            val response = dogApiService.getBreeds()
+            _contactsViewState.value = ContactsViewState(
+                contacts = response.data.mapIndexed { index, singleResponseData ->
+                    generateContact(index.toLong()).copy(name = singleResponseData.attributes.name)
+                }
+            )
+        }
     }
 
     fun onContactSelected(contact: Contact?) {
+        refresh()
         Log.d("MainViewModel", "onContactSelected: $contact")
         _contactsViewState.value = _contactsViewState.value.copy(selectedContact = contact)
     }
