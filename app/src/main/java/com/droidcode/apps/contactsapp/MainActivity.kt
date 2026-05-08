348354a5ad5770f709ffc9067d24a93012ee2fa7
tomaszmaslon <tomasz.maslon@gmail.com>
2026-05-08 10:15:33 +0200

    Added navigation with use of navigation controller

--- app/src/main/java/com/droidcode/apps/contactsapp/MainActivity.kt
+++ app/src/main/java/com/droidcode/apps/contactsapp/MainActivity.kt
@@ -8,6 +8,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.getValue
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.droidcode.apps.contactsapp.ui.contacts.ContactPreviewScreen
 import com.droidcode.apps.contactsapp.ui.contacts.ContactsListScreen
 import com.droidcode.apps.contactsapp.ui.theme.ContactsAppTheme
@@ -20,8 +23,37 @@ class MainActivity : ComponentActivity() {
         setContent {
             val viewModel: MainViewModel = viewModel()
             val contactState by viewModel.contacts.collectAsStateWithLifecycle()
+            val navController = rememberNavController()
 
             ContactsAppTheme {
+                NavHost(
+                    navController = navController,
+                    startDestination = "contactsList"
+                ) {
+                    composable("contactsList") {
+                        ContactsListScreen(
+                            contactState.contacts,
+                            onContactSelected = {
+                                viewModel.selectContact(it)
+                                navController.navigate("contactPreview")
+                            }
+                        )
+                    }
+                    composable("contactPreview") {
+                        contactState.selected?.let {
+                            ContactPreviewScreen(
+                                contact = it,
+                                onBackClick = {
+                                    viewModel.unselectContact()
+                                    navController.popBackStack()
+                                }, onCallContactClick = {
+                                }
+                            )
+                        }
+                    }
+                }
+
+
                 if (contactState.selected != null) {
                     Log.i("MainActivity", "Selected contact: ${contactState.selected?.firstName}")
                     ContactPreviewScreen(
