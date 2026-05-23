1c859d1ebf33dba5d2986eaf1c5edc4da285f244
tomaszmaslon <tomasz.maslon@gmail.com>
2026-05-23 09:14:47 +0200

    Added navigation in Compose

--- app/src/main/java/com/droidcode/apps/contactsapp/MainActivity.kt
+++ app/src/main/java/com/droidcode/apps/contactsapp/MainActivity.kt
@@ -8,6 +8,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.getValue
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.droidcode.apps.contactsapp.ui.contacts.ContactPreviewScreen
 import com.droidcode.apps.contactsapp.ui.contacts.ContactsListScreen
 import com.droidcode.apps.contactsapp.ui.theme.ContactsAppTheme
@@ -20,14 +23,29 @@ class MainActivity : ComponentActivity() {
         setContent {
             val viewModel: MainViewModel = viewModel()
             val contactState by viewModel.contacts.collectAsStateWithLifecycle()
+            val navController = rememberNavController()
 
             ContactsAppTheme {
-                ContactsListScreen(
-                    contactState.contacts,
-                    onContactSelected = {
-                        viewModel.selectContact(it)
-                    })
-//                ContactPreviewScreen(contactState.contacts[0])
+                NavHost(
+                    navController = navController,
+                    startDestination = "contacts_list"
+                ) {
+                    composable("contacts_list") {
+                        ContactsListScreen(
+                            contactState.contacts,
+                            onContactSelected = {
+                                viewModel.selectContact(it)
+                                navController.navigate("contact_preview")
+                            }
+                        )
+                    }
+                    composable("contact_preview") {
+                        if (contactState.selected != null) {
+                            val contact = contactState.selected!!
+                            ContactPreviewScreen(contact)
+                        }
+                    }
+                }
             }
         }
     }
