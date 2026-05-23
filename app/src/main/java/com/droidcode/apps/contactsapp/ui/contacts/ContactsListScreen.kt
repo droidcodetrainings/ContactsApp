305ee48f1df453b33ef65cda43c68e3fbaadd786
tomaszmaslon <tomasz.maslon@gmail.com>
2026-05-23 13:06:33 +0200

    Added navigation with if statement

--- app/src/main/java/com/droidcode/apps/contactsapp/ui/contacts/ContactsListScreen.kt
+++ app/src/main/java/com/droidcode/apps/contactsapp/ui/contacts/ContactsListScreen.kt
@@ -1,5 +1,6 @@
 package com.droidcode.apps.contactsapp.ui.contacts
 
+import androidx.activity.compose.BackHandler
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
