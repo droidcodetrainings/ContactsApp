e407cd3e1b2f155f58ebecb6b97ee64b31de7365
tomaszmaslon <tomasz.maslon@gmail.com>
2026-05-23 15:23:19 +0200

    Added navigation jetpack compose for screens

--- app/src/main/java/com/droidcode/apps/contactsapp/MainActivity.kt
+++ app/src/main/java/com/droidcode/apps/contactsapp/MainActivity.kt
@@ -8,6 +8,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.getValue
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.droidcode.apps.contactsapp.ui.contacts.ContactPreviewScreen
 import com.droidcode.apps.contactsapp.ui.contacts.ContactsListScreen
 import com.droidcode.apps.contactsapp.ui.theme.ContactsAppTheme
@@ -20,14 +23,34 @@ class MainActivity : ComponentActivity() {
         setContent {
             val viewModel: MainViewModel = viewModel()
             val contactState by viewModel.contacts.collectAsStateWithLifecycle()
+            val navController = rememberNavController()
 
             ContactsAppTheme {
-                ContactsListScreen(
-                    contactState.contacts,
-                    onContactSelected = {
-                        viewModel.selectContact(it)
-                    })
-//                ContactPreviewScreen(contactState.contacts[0])
+                NavHost(
+                    navController = navController,
+                    startDestination = "contactsList"
+                ) {
+                    composable("contactsList") {
+                        ContactsListScreen(
+                            contactState.contacts,
+                            onContactSelected = {
+                                viewModel.selectContact(it)
+                                navController.navigate("contactPreview")
+                            }
+                        )
+                    }
+                    composable("contactPreview") {
+                        contactState.selected?.let { contact ->
+                            ContactPreviewScreen(
+                                contact = contact,
+                                onBackClick = {
+                                    viewModel.unselectContact()
+                                    navController.popBackStack()
+                                }
+                            )
+                        }
+                    }
+                }
             }
         }
     }
--- app/src/main/java/com/droidcode/apps/contactsapp/MainViewModel.kt
+++ app/src/main/java/com/droidcode/apps/contactsapp/MainViewModel.kt
@@ -20,6 +20,10 @@ class MainViewModel : ViewModel() {
         Log.i("MainActivity", "Selected contact: ${contact.firstName}")
     }
 
+    fun unselectContact() {
+        _contacts.value = _contacts.value.copy(selected = null)
+    }
+
     private fun generateContactItems(): List<Contact> {
         return List(100) { index ->
             val number = index + 1
@@ -35,5 +39,6 @@ class MainViewModel : ViewModel() {
             )
         }
     }
+
 
 }
