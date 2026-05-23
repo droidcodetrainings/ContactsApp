af16725cd5645fb76e481e527b26155c8b972f9b
tomaszmaslon <tomasz.maslon@gmail.com>
2026-05-23 11:03:19 +0200

    Added navigation with jetpack compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     implementation(libs.coil.compose)
     implementation(libs.play.services.maps)
     implementation(libs.joda.time)
+    implementation(libs.androidx.navigation.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
